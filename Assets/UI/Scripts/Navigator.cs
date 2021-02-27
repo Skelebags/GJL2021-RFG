84922c4d8b635ea5aff0945af394b19f8c61f3fe
AegisRising1 <44650640+AegisRising1@users.noreply.github.com>
2021-02-27 22:06:59 +0000

    Map/Navigation.

---
+++ Assets/UI/Scripts/Navigator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Navigator : MonoBehaviour
+{
+    public void ToMap()
+    {
+        SceneManager.LoadScene("Map");
+    }
+
+    public void ToPlant_ShopScene()
+    {
+        SceneManager.LoadScene("Plant_ShopScene");
+    }
+
+    public void ToMeat_ShopScene()
+    {
+        SceneManager.LoadScene("Meat_Shopscene");
+    }
+
+    public void ToMineral_ShopScene()
+    {
+        SceneManager.LoadScene("Mineral_Shopscene");
+    }
+
+    public void ToMain()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}
