7e0f9ab517efaa364591fa97c00d3de4abc24a08
tailzdafox <tailzdafox@gmail.com>
2021-02-28 17:08:37 +0000

    THE NAMES THEY BE REAL

---
+++ Assets/UI/Scripts/LoadButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadButton : MonoBehaviour
+{
+    public GameObject newGameScreen;
+    public Navigator navigator;
+
+    private PlayerDataManager player;
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("GameManager").GetComponent<PlayerDataManager>();
+    }
+
+    public void NewOrLoad()
+    {
+        Debug.Log(player.player_name);
+        if(player.player_name == "NO SAVE DATA")
+        {
+            newGameScreen.SetActive(true);
+        }
+        else
+        {
+            navigator.ToMap();
+        }
+    }
+}
---
+++ Assets/UI/Scripts/NameInput.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NameInput : MonoBehaviour
+{
+    private PlayerDataManager player;
+    public TMPro.TMP_InputField input;
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("GameManager").GetComponent<PlayerDataManager>();
+    }
+
+    public void SetPlayerName()
+    {
+        player.player_name = input.text;
+    }
+}
